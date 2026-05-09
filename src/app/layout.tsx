87e5fb3f0e2fc5749190552b941a14c85cfc49d9
Uves Khan <Uveskhanx@gmail.com>
2026-05-09 08:09:32 +0530

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { Providers } from './providers';
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({
   subsets: ['latin'],
@@ -86,6 +87,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Providers>
           {children}
           <SpeedInsights />
+          <Analytics />
         </Providers>
       </body>
     </html>
